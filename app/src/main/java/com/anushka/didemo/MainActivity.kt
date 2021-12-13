e80311a79e04bf9c4148db4acef965936091902a
EvgeniyBelaga <evgeniybelaga79@gmail.com>
2021-12-13 19:29:16 +0200

    DIDemo 12.13.21

---
+++ app/src/main/java/com/anushka/didemo/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.anushka.didemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var smartPhone: SmartPhone
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val battery= Battery()
+//
+//        val memoryCard= MemoryCard()
+//
+//        val serviceProvider= ServiceProvider()
+//
+//        val simCard= SIMCard(serviceProvider)
+//
+//        val smartPhone= SmartPhone(battery, simCard, memoryCard)
+
+
+
+//        val smartPhone= SmartPhone(Battery(), SIMCard(ServiceProvider()), MemoryCard())
+//
+//        smartPhone.makeACallWithRecording()
+
+
+        DaggerSmartPhoneComponent.create().getSmartPhone().makeACallWithRecording()
+
+    }
+}
